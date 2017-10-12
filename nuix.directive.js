aa4cb479f77f3eeac77b52aebfe13685f9604a67
Abdul Wahab <ab.1303@gmail.com>
2017-10-12 12:03:41 +1100

    Second Commit

--- nuix.directive.js
+++ nuix.directive.js
@@ -1,13 +1,5 @@
 
-angular.module('nuixApp').directive('nuixTree',['RecursionHelper',function(recursionHelper){
-    function treeController(){
-        var vm = this;
-        vm.isRootNode = vm.nodeType && vm.nodeType === 'root';
-        vm.isLeafNode = vm.treeMap.children.length===0;
-        vm.isBranchNode = !vm.isLeafNode;        
-    }
-
-    
+angular.module('nuixApp').directive('nuixTree', ['RecursionHelper', function (recursionHelper) {
     return {
         restrict: "E",
         replace:true,
@@ -18,7 +10,7 @@ angular.module('nuixApp').directive('nuixTree',['RecursionHelper',function(recur
         controllerAs: 'vm',
         bindToController: true,
         templateUrl: 'nuix.directive.template.html',
-        controller:treeController,
+        controller: treeController,
         compile: function(element) {
             return recursionHelper.compile(element, function(scope, iElement, iAttrs, controller, transcludeFn){
                 // Define your normal link function here.
@@ -30,3 +22,28 @@ angular.module('nuixApp').directive('nuixTree',['RecursionHelper',function(recur
     };
 
 }]);
+
+angular.module('nuixApp').controller('treeController', treeController);
+treeController.$inject = ['$scope'];
+
+function treeController($scope) {
+    var vm = this;
+    vm.isRootNode = vm.nodeType && vm.nodeType === 'root';
+    vm.isLeafNode = vm.treeMap.children.length === 0;
+    vm.isBranchNode = !vm.isLeafNode;
+
+
+    vm.emitEvent = function () {
+        $scope.$emit('traverseTree', {
+            type: vm.treeMap.type,
+            label: vm.treeMap.label
+        });
+    };
+
+    $scope.$on('traverseTree', function (event, data) {
+        var currentScopeVm = event.currentScope.vm;
+        var text = currentScopeVm.treeMap.type + ' - ' + currentScopeVm.treeMap.label;
+        console.log(text);
+    });
+
+}
